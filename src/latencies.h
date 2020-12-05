e79d73cf5e6e5240cf3258cb290b9ea75139b363
Sam Flattery <sflatter@andrew.cmu.edu>
2020-12-05 14:02:42 +0000

    add latency sources

--- src/latencies.h
+++ src/latencies.h
@@ -7,6 +7,7 @@ static const int NUMA_DISTANCE = 2
 
 
 // L1 cache hit time
+// https://stackoverflow.com/questions/3142779/how-much-time-does-it-take-to-fetch-one-word-from-memory
 static const int CACHE_TIME = 1
 
 // main memory read time
@@ -15,6 +16,7 @@ static const int MEMORY_TIME = 100
 // directory -> cache and cache->directory latency
 // Intel's interconnect has operates at ~5GT/s (gigatransfers per second), so each transfer takes
 // about 1/(10^9)s = 1ns
+// https://en.wikipedia.org/wiki/Intel_QuickPath_Interconnect#Frequency_specifications
 // this is probably not super accurate but we can change it if we get better info
 static const int LOCAL_INTERCONNECT = 1
 
