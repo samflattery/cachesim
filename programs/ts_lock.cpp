2899ede581f98810eb0e6b3c59542b0473a54325
Sam Flattery <sflatter@andrew.cmu.edu>
2020-12-10 14:03:32 -0500

    make n threads instead of n-1

--- programs/ts_lock.cpp
+++ programs/ts_lock.cpp
@@ -41,7 +41,7 @@ int main(int argc, char *argv[]) {
   }
   int num_threads = atoi(argv[1]);
   vector<thread> thrList;
-  for (int i = 1; i < num_threads; i++) {
+  for (int i = 0; i < num_threads; i++) {
     thrList.push_back(thread(incr, 100));
   }
   for (auto &t : thrList) t.join();
